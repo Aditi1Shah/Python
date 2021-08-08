2f633368ebb62035b5bd3e85f8b14dce7a1bac93
Aditi Shah <72251182+Aditi1Shah@users.noreply.github.com>
2021-08-08 23:35:16 +0530

    Initial Commit

---
+++ Function/function_def.py
@@ -0,0 +1,9 @@
+def greeting(name, department):
+    print("Welcome "+ name)         #Notice the tabs here its imp to give tabs as we arent using brackets in python
+    print("Your are a part of "+ department)
+
+greeting("Alisha","IT Support")
+greeting("Roy","Developer Team")
+
+#Python is a strongly-typed dynamic language in which we don't have to specify the data type of the function 
+# return value and function argument. It relates type with values instead of names. 
